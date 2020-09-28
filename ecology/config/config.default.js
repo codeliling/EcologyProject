d8e94932ea79f9093b4a68a76a0d921bbb06b7a7
codeliling <maximuslee@126.com>
2020-09-28 15:28:03 +0800

    project init

---
+++ ecology/config/config.default.js
@@ -0,0 +1,69 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1601267004036_3921';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  config.sequelize = {
+    dialect: 'mysql',
+    host: '192.168.3.101',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'ecology',
+    logging:true,
+    timezone: '+08:00',
+    define: {
+      freezeTableName: true,
+      charset: 'utf8',
+      dialectOptions: {
+        collate: 'utf8_general_ci',
+      },
+      timestamps: false,
+
+    },
+    pool: {
+      max: 5,
+      min: 1,
+      acquire: 30000,
+      idle: 10000
+    },
+  };
+
+  config.cors = {
+    origin:'*',
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH'
+  };
+
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+  };
+
+  config.assets = {
+    publicPath: '/public/',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
---
+++ ecology/config/plugin.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/** @type Egg.EggPlugin */
+module.exports = {
+
+  sequelize:{
+    enable: true,
+    package: 'egg-sequelize',
+  },
+
+  nunjucks:{
+    enable: true,
+    package: 'egg-view-nunjucks',
+  },
+};
