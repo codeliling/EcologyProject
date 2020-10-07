79947c08f87a0bcb2538e0b80d671968d80308b2
codeliling <maximuslee@126.com>
2020-10-07 08:40:33 +0800

    code

--- ecology/app/public/js/src/mine/air.js
+++ ecology/app/public/js/src/mine/air.js
@@ -50,6 +50,14 @@ var app = new Vue({
     monthAirBarData:[],
     yearAirBarData:[],
 
+    lineWeekTimeData:["星期天(11号)","星期一(12号)","星期二(13号)","星期三(14号)","星期四(15号)","星期五(16号)","星期六(17号)"],
+    lineWeekData:[88,91,90,89,89,91,90],
+    lineMonthTimeData:["1号","2号","3号","4号","5号","6号","7号","8号","9号","10号","11号","12号","13号","14号","15号","16号","17号"],
+    lineMonthData:[90,91,90,89,87,88,91,89,90,90,88,91,90,89,89,91,90],
+    lineYearTimeData:["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月"],
+    lineYearData:[88,88,88,88,89,88,89,91,89,90],
+    airLineGraphic:{},
+    airLineOption:{},
   },
   methods:{
     dayBtnclick:function(){
@@ -97,6 +105,21 @@ var app = new Vue({
       }
       this.airBarGraphic.setOption(this.airBarOption);
     },
+    lineWeekBtnclick:function(){
+      this.airLineOption.xAxis[0].data = this.lineWeekTimeData;
+      this.airLineOption.series[0].data = this.lineWeekData;
+      this.airLineGraphic.setOption(this.airLineOption);
+    },
+    lineMonthBtnClick:function(){
+      this.airLineOption.xAxis[0].data = this.lineMonthTimeData;
+      this.airLineOption.series[0].data = this.lineMonthData;
+      this.airLineGraphic.setOption(this.airLineOption);
+    },
+    lineYearBtnClick:function(){
+      this.airLineOption.xAxis[0].data = this.lineYearTimeData;
+      this.airLineOption.series[0].data = this.lineYearData;
+      this.airLineGraphic.setOption(this.airLineOption);
+    }
   },
   mounted() {
     var myChart = echarts.init(document.getElementById('map'));
@@ -330,18 +353,9 @@ var app = new Vue({
     this.airBarGraphic.setOption(this.airBarOption);
 
     //------------------------------------------------------------------------------------
-    var airLineGraphic = echarts.init(document.getElementById('air-line'));
+    this.airLineGraphic = echarts.init(document.getElementById('air-line'));
 
-    var data = [["2000-06-05",116],["2000-06-06",129],["2000-06-07",135],["2000-06-08",86],["2000-06-09",73],["2000-06-10",85],["2000-06-11",73],["2000-06-12",68],["2000-06-13",92],["2000-06-14",130],["2000-06-15",245],["2000-06-16",139],["2000-06-17",115],["2000-06-18",111],["2000-06-19",309],["2000-06-20",206],["2000-06-21",137],["2000-06-22",128],["2000-06-23",85],["2000-06-24",94],["2000-06-25",71],["2000-06-26",106],["2000-06-27",84],["2000-06-28",93],["2000-06-29",85],["2000-06-30",73],["2000-07-01",83],["2000-07-02",125],["2000-07-03",107],["2000-07-04",82],["2000-07-05",44],["2000-07-06",72],["2000-07-07",106],["2000-07-08",107],["2000-07-09",66],["2000-07-10",91],["2000-07-11",92],["2000-07-12",113],["2000-07-13",107],["2000-07-14",131],["2000-07-15",111],["2000-07-16",64],["2000-07-17",69],["2000-07-18",88],["2000-07-19",77],["2000-07-20",83],["2000-07-21",111],["2000-07-22",57],["2000-07-23",55],["2000-07-24",60]];
-
-    var dateList = data.map(function (item) {
-        return item[0];
-    });
-    var valueList = data.map(function (item) {
-        return item[1];
-    });
-
-    airLineOption = {
+    this.airLineOption = {
 
         // Make gradient line here
         visualMap: [{
@@ -355,13 +369,13 @@ var app = new Vue({
 
         title: [{
             left: 'center',
-            text: 'Gradient along the y axis'
+            text: '大气总体趋势图'
         }],
         tooltip: {
             trigger: 'axis'
         },
         xAxis: [{
-            data: dateList
+            data: this.lineWeekTimeData
         }],
         yAxis: [{
             splitLine: {show: false}
@@ -374,11 +388,11 @@ var app = new Vue({
         series: [{
             type: 'line',
             showSymbol: false,
-            data: valueList
+            data: this.lineWeekData
         }]
     };
 
-    airLineGraphic.setOption(airLineOption);
+    this.airLineGraphic.setOption(this.airLineOption);
 
   },
   created() {
